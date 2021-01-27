03eb79ea53a9a7606bc324c400b55f4203bbbafe
Mizeck Victor Tembo <51457834+Mizeck@users.noreply.github.com>
2021-01-27 17:41:05 +0200

    Update db.js

--- server/Model/connectionDb/db.js
+++ server/Model/connectionDb/db.js
@@ -2,7 +2,7 @@ const {Pool} = require("pg")
 
 const pool = new Pool({
     user :"postgres",
-    password: "bit1622063", 
+    password: "password", 
     host : "localhost",
     port: 5432,
     database:"todo_appdb"
